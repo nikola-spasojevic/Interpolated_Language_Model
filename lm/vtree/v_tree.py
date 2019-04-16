34a540b4bc66bb29a1984696984eae02f6ff14db
Nikola Spasojevic <nikola.t.spasojevic@gmail.com>
2019-04-16 09:52:27 +0200

    implemented vocabulary tree for next wrod prediction

--- lm/vtree/v_tree.py
+++ lm/vtree/v_tree.py
@@ -4,8 +4,9 @@ class VNode:
 		self.children = {}
 
 class VTree:
-	def __init__(self, threshold=0.0002):
+	def __init__(self, unk_score, threshold=0.0002):
 		self.root = VNode()
+		self.unk_score = unk_score
 		self.threshold = threshold
 
 	def insert(self, target_word, lklhd, context=()):
@@ -18,15 +19,13 @@ class VTree:
 			curr = node
 		curr.children[target_word] = VNode(lklhd)
 
-	def get_likelihood(self, word, context=None):
+	def get_likelihood(self, target_word, context=()):
 		curr = self.root
-		if not context:
-			unigram = curr.children.get(word)
-			return unigram.lklhd if unigram else None
-		for w in context:
+		for word in context:
 			node = curr.children.get(word)
 			if not node:
-				# lower model by 1 degree - backoff model
-				return self.get_likelihood(word, context[1:])
-
-
+				# Backoff Model - lower ngram by 1 degree
+				c_lst = list(context)
+				return self.get_likelihood(target_word, tuple(c_lst[1:]))
+		node = curr.children.get(target_word)
+		return self.unk_score if not node else node.lklhd
