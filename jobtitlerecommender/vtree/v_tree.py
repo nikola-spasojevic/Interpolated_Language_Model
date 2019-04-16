a2f0f7cbf03af0a5e35a9cc18981e8d59a30cc26
Nikola Spasojevic <nikola.t.spasojevic@gmail.com>
2019-04-16 16:05:40 +0200

    fixed lklhd calculation for vtree

--- jobtitlerecommender/vtree/v_tree.py
+++ jobtitlerecommender/vtree/v_tree.py
@@ -1,14 +1,9 @@
+from collections import defaultdict
+
 class VNode:
 	def __init__(self, lklhd=0.0):
 		self.lklhd = lklhd
-		self.children = {}
-
-	def all_words(self, word=''):
-		if not self.children:
-			yield word
-
-		for word, child in self.children.items():
-			yield from child.all_words(word)
+		self.children = defaultdict(VNode)
 
 class VTree:
 	def __init__(self, unk_score, threshold=0.0002):
@@ -19,12 +14,9 @@ class VTree:
 	def insert(self, target_word, lklhd, context=()):
 		curr = self.root
 		for word in context:
-			node = curr.children.get(word)
-			if not node:
-				node = VNode()
-				curr.children[word] = node
+			node = curr.children[word]
 			curr = node
-		curr.children[target_word] = VNode(lklhd)
+		curr.children[target_word].lklhd = lklhd
 
 	def get_likelihood(self, target_word, context=()):
 		curr = self.root
@@ -34,19 +26,25 @@ class VTree:
 				# Backoff Model - lower ngram by 1 degree
 				c_lst = list(context)
 				return self.get_likelihood(target_word, tuple(c_lst[1:]))
-			curr = node
 		node = curr.children.get(target_word)
 		return self.unk_score if not node else node.lklhd
 
+	def print_levels(self):
+		for k1, v1 in self.root.children.items():
+			print('first_level: {}, {}'.format(k1, v1.lklhd))
+			for k2, v2 in v1.children.items():
+				print('second_level: {}, {}'.format(k2, v2.lklhd))
+				for k3, v3 in v2.children.items():
+					print('third_level: {}, {}'.format(k3, v3.lklhd))
+			print()
+
 	def word_prediction(self, context=()):
 		curr = self.root
-		print(context)
 		for word in context:
 			node = curr.children.get(word)
 			if not node:
-				print('Backoff')
 				# Backoff Model - lower ngram by 1 degree
 				c_lst = list(context)
 				return self.word_prediction(tuple(c_lst[1:]))
-
-		return node.children.items()
+			curr = node
+		return [(node.lklhd, word) for word, node in curr.children.items()]
